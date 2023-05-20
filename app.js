0c7efad187ae853a5d74cf518e6faa85763cb059
Yu-Jhen Wu <j0960087026@gmail.com>
2023-05-20 19:54:11 +0800

    update app.js

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+//app.use(express.static(path.join(__dirname, 'public')));
 // 在 app.js 中設定載入模組 express-session
 app.use(session({
   secret: 'keyboard cat',
